e8b97894106fae9c64d93c3712988d1e1b645dec
André Panisson <panisson@gmail.com>
2012-09-07 22:54:07 -0700

    Add empty configuration

---
+++ whatshot/config.py
@@ -0,0 +1,23 @@
+# coding: utf-8
+#
+#  Copyright (C) 2012 André Panisson
+#  You can contact me by email (panisson@gmail.com) or write to:
+#  Via Alassio 11/c - 10126 Torino - Italy
+#
+#
+
+# Database configuration
+USERNAME=''
+PASSWORD=''
+HOST=''
+DB=''
+
+# Flask configuration
+SECRET_KEY = ''
+DEBUG = True
+
+# OAuth configuration
+CONSUMER_KEY=''
+CONSUMER_SECRET=''
+
+STOPWORDS_FILE='StopWords.txt'
